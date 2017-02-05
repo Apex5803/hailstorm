19ca36c0bb333ea569d36eb1f89483d040389708
Apex5803 <JJ.latanom42@gmailcom>
2017-02-04 16:00:47 -0800

    We finished drive code finally!!!!! Changes- changed intitialization in MuitiSpeedController to use ports, updated everything involved with the drive Base speed controllers, added initMotors method inMultiSpeedController
    Darth Jar Jar and company out

--- src/Custom/MultiSpeedController.cpp
+++ src/Custom/MultiSpeedController.cpp
@@ -6,50 +6,72 @@
  */
 
 #include <Custom/MultiSpeedController.h>
+#include "WPILib.h"
 
-MultiSpeedController::MultiSpeedController(std::shared_ptr<SpeedController> speedControllers[], int size) {
+MultiSpeedController::MultiSpeedController(int motor_ports[], int size) {
 	// TODO Auto-generated constructor stub
 	this->currentSpeed = .0;
-	this->speedControllers = new std::vector<std::shared_ptr<SpeedController>>();
-	for(int i = 0; i > size; i++){
-			this->speedControllers.push_back(speedControllers[i]);
-		}
 	this->isInverted = false;
+	this->num_motors = size;
+	this->motor_ports = new int[size];
+	for (int i = 0; i < num_motors; i++){
+		this->motor_ports[i] = motor_ports[i];
+	}
 
 }
 
+ MultiSpeedController::~MultiSpeedController(){
+	delete[] this->motor_ports;
+}
+
+
+void MultiSpeedController::InitMotors(){
+	for(int i = 0; i < num_motors; i++){
+				std::shared_ptr<SpeedController> new_motor;
+				new_motor.reset(new VictorSP(motor_ports[i]));
+				this->speedControllers.emplace_back(new_motor);
+			}
+}
+
 void MultiSpeedController::Set(double speed){
-	for(int i = 0; i > this->speedControllers.size; i++){
+	for(unsigned int i = 0; i < this->speedControllers.size(); i++){
 			this->speedControllers[i]->Set(speed);
 	}
 
 	this->currentSpeed = speed;
 }
 
-double MultiSpeedController::Get(){
+double MultiSpeedController::Get() const{
 	return currentSpeed;
 }
 
 void MultiSpeedController::SetInverted (bool isInverted){
-	for(int i = 0; i > this->speedControllers.size; i++)
+	for(unsigned int i = 0; i < this->speedControllers.size(); i++)
 	{
 		this->speedControllers[i]->SetInverted(isInverted);
 	}
 	this-> isInverted = isInverted;
 }
 
+void MultiSpeedController::PIDWrite(double output){
+	for(unsigned int i = 0; i < this->speedControllers.size(); i++)
+	{
+		this->speedControllers[i]->PIDWrite(output);
+	}
+}
+
 void MultiSpeedController::Disable(){
-	for(int i = 0; i > this->speedControllers.size; i++){
+	for(unsigned int i = 0; i < this->speedControllers.size(); i++){
 		this->speedControllers[i]->Disable();
 	}
 }
 
-bool MultiSpeedController::GetInverted(){
+bool MultiSpeedController::GetInverted() const{
 	return isInverted;
 }
 
 void MultiSpeedController::StopMotor(){
-	for(int i = 0; i > this->speedControllers.size; i++){
+	for(unsigned int i = 0; i < this->speedControllers.size(); i++){
 			this->speedControllers[i]->StopMotor();
 		}
 }
--- src/Custom/MultiSpeedController.h
+++ src/Custom/MultiSpeedController.h
@@ -8,22 +8,27 @@
 #ifndef SRC_CUSTOM_MULTISPEEDCONTROLLER_H_
 #define SRC_CUSTOM_MULTISPEEDCONTROLLER_H_
 #include "WPILib.h"
-
+#include <VictorSP.h>
 
 class MultiSpeedController: public SpeedController {
 private:
 	std::vector<std::shared_ptr<SpeedController>> speedControllers;
 	double currentSpeed;
 	bool isInverted;
+	int *motor_ports;
+	int num_motors;
 
 public:
-	MultiSpeedController(std::shared_ptr<SpeedController> speedControllers[], int size);
-	void Set(double speed);
-	double Get();
-	void SetInverted(bool isInverted);
-	void Disable();
-	bool GetInverted();
-	void StopMotor();
+	MultiSpeedController(int motor_ports[], int size);
+	~MultiSpeedController();
+	void InitMotors();
+	void Set(double speed) override;
+	double Get() const override;
+	void SetInverted(bool isInverted) override;
+	void Disable() override;
+	bool GetInverted() const override;
+	void StopMotor() override;
+	void PIDWrite(double output) override;
 };
 
 #endif /* SRC_CUSTOM_MULTISPEEDCONTROLLER_H_ */
