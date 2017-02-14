afb66f8db7589d80f88b84402ab518858c5f92c3
Eli <egirod@gmail.com>
2017-02-13 20:13:16 -0800

    Syncing local workstation to cloud. Should be no changes

--- src/Vision/VisionProcessing.cpp
+++ src/Vision/VisionProcessing.cpp
@@ -6,57 +6,73 @@
  */
 
 #include <Vision/VisionProcessing.h>
+#include <Vision/VisionDataResult.h>
 #include <math.h> //we need math!
+#include <iostream>
 
-VisionDataResult VisionProcessing::VisionProcessing() {
+VisionDataResult VisionProcessing::GetRawData() {
 
-	std::shared_ptr<NetworkTable> table;
 	table = NetworkTable::GetTable("GRIP/myContoursReport");
 
-
 	//read values into a local array
-	std::vector<double> arr = table->GetNumberArray("area", llvm::ArrayRef<double>());
-	std::vector<double> x = table->GetNumberArray("centerX", llvm::ArrayRef<double>());
-	std::vector<double> y = table->GetNumberArray("centerY", llvm::ArrayRef<double>());
+	arr = table->GetNumberArray("area", llvm::ArrayRef<double>());
+	x = table->GetNumberArray("centerX", llvm::ArrayRef<double>());
+	y = table->GetNumberArray("centerY", llvm::ArrayRef<double>());
 
 
 	//look for targets with a minimum area (or this can be done in GRIP filtering)
-	double largest = 0; //initialize largest target
-	double largestX = 0;
-	double largestY = 0;
-	double secondLargest = 0; //initialize 2nd largest target --> we may not have to use this right now
-	double secondLargestX = 0;
-	double secondLargestY = 0;
-	double yDistance = 0; //declare a large nunber
+	largest = 0; //initialize largest target
+	largestX = 0;
+	largestY = 0;
+	secondLargest = 0; //initialize 2nd largest target --> we may not have to use this right now
+	secondLargestX = 0;
+	secondLargestY = 0;
+	yDistance = 0; //declare a large number
+
 
 	for (unsigned int i=0; i < arr.size(); i++){
 
-		if(arr(i) > largest) //find the largest array, record the size and X Y Position
+		if(arr[i]> largest) //find the largest array, record the size and X Y Position
 			{
-				largest=arr(i);
-				largestX = x(i);
-				largestY = y(i);
+				largest=arr[i];
+				largestX = x[i];
+				largestY = y[i];
 			}
-		if(arr(i)> secondLargest & arr(i) < largest) //if the current area is bigger than existing second largest but smaller than the largest, record it
-		{
-			secondLargest = arr(i);
-			secondLargestX = x(i);
-			secondLargestY = y(i);
-		}
+		if((arr[i]> secondLargest) & (arr[i] < largest)) //if the current area is bigger than existing second largest but smaller than the largest, record it
+			{
+				secondLargest = arr[i];
+				secondLargestX = x[i];
+				secondLargestY = y[i];
+			}
 
 		yDistance = abs(largestY - secondLargestY); //get the absolute value of the distance between the Y points
-
-	}
-
+		}
 
 	//Return the position of the largest target
-	return new VisionDataResult(largest, largestX, largestY);
+	return VisionDataResult(largest, largestX, largestY);
+}
 
-	}
+std::tuple<double,double> VisionProcessing::GetScaledData(double Xpixels, double Ypixels){
+
+	//Heading here is from -1 to 1 where 0 is the center of the frame (no position adjustment)
+    double heading=0; //initialize set point to center of the frame
+    double pitch=0;
+	//The heading formula is H = (Px - Xresolution/2)/(Xresolution/2)
+
+    //Set the camera image resolution
+	//Positions in the camera are pixels relative to the resolution of the camera. (0,0) is the upper left camera
+	double Xresolution = 320;
+	double Yresolution = 160;
+
+	//convert pixels to heading
+	heading = (Xpixels - Xresolution/2)/(Xresolution/2);
+	pitch 	= (Ypixels - Yresolution/2)/(Yresolution/2);
+
+	return std::make_tuple(heading,pitch);
 
 }
 
-VisionProcessing::~VisionProcessing() {
+//VisionDataResult VisionProcessing::~GetNewData() { //Do we need this destructor??
 	// TODO Auto-generated destructor stub
-}
+//}
 
--- src/Vision/VisionProcessing.h
+++ src/Vision/VisionProcessing.h
@@ -7,19 +7,29 @@
 
 #ifndef SRC_VISION_VISIONPROCESSING_H_
 #define SRC_VISION_VISIONPROCESSING_H_
+#include <WPILib.h>
+#include <Vision/VisionDataResult.h>
+#include <iostream>
 
 class VisionProcessing {
 public:
-	VisionProcessing(){
-		table = NetworkTable::GetTable("GRIP/myContoursReport");
-	}
-
-	std::shared_ptr<NetworkTable> table;
+	VisionDataResult GetRawData(); //Get new data from the GRIP vision system NetworkTables.This will return Area, X, and Y Position of the largest contour found
+	std::tuple<double,double> GetScaledData(double Xpixels, double Ypixels); //Convert pixels a heading and pitch setpoint (pixels to heading -1 to 1)
 
 private:
 	std::vector<double> arr;
 	std::vector<double> x;
 	std::vector<double> y;
+
+	std::shared_ptr<NetworkTable> table;
+
+	double largest; //initialize largest target
+	double largestX;
+	double largestY;
+	double secondLargest; //initialize 2nd largest target --> we may not have to use this right now
+	double secondLargestX;
+	double secondLargestY;
+	double yDistance; //declare a large number
 };
 
 #endif /* SRC_VISION_VISIONPROCESSING_H_ */
